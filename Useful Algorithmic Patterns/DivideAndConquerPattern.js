59ed25f6f785b6bfc39b280dfb4c36578a671f1f
codeprince <anyaohaprince122@gmail.com>
2021-03-25 12:12:51 +0100

    added sliding window pattern and explained divide and conquer

---
+++ Useful Algorithmic Patterns/DivideAndConquerPattern.js
@@ -0,0 +1,3 @@
+// this is basicaly a pattern that involves breaking the solution to a problem
+// into different parts and litterally conquer them or in a more conventional term, 'solve' them
+// check out binary sort algorithm in the sorting algorithms folder for an example of this pattern
--- Useful Algorithmic Patterns/SlidingWindowPattern.js
+++ Useful Algorithmic Patterns/SlidingWindowPattern.js
@@ -1,19 +1,36 @@
 // this pattern involves creating a window and moving the window based on a condition
+// next function is a function that takes an array and a number n as parameters 
+// and checks for the maximum value we would get by adding any n elements
+// so if n is 5 then it will check the maximum value we would get by adding 5 elements in the array
 
 const maxSubArraySum = (array, num) => {
+  // first check for edge case where the number of elements to add is greater 
+  // than the number of elements in the array
   if(array.length < num) return null;
+
+  // initialize two variables to store the max value whilw the calculation happens
   let max = 0;
   let tempmax = 0;
 
+  // find the sum of the first num elements
   for(let i = 0; i < num; i++) {
     max += array[i];
   }
 
+  // loop through the array and move forward by adding the next element
+  // while substracting the element at the back, also we dont start from the begining of the array for two reasons
+  // one, because we have already added the first num elements in the loop above
+  // and two, because we want the last addition to be from the element at the 
+  // difference between the length of the array and num so that there are enough supplies of elements to add
   for(let i = num; i < array.length; i++) {
     tempmax = max + array[i] - array[i - num]
+
+    // reset max to the maximun between max and the current sum
     max = Math.max(tempmax, max)
   }
-  console.log(max)
+  return max
 }
 
-maxSubArraySum([1,2,3,4,5], 3)
+// as you might have noticed, this pattern is like a sliding window, 
+// if you dont understand, you can hit me up at anyaohaprince122@gmail.com
+// i will be happy to help you understand
