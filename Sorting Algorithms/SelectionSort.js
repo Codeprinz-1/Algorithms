a4ae6ffb49db976ae9a62ab6801f50becdc49b77
codeprince <anyaohaprince122@gmail.com>
2021-04-18 09:50:29 +0100

    added selection sort

---
+++ Sorting Algorithms/SelectionSort.js
@@ -0,0 +1,20 @@
+// selection sort is kinda similar to buble sort, it involves checking for the lowest item in the array
+// and setting it to the beggining, but unlike bubble sort it doesnt swap as it goes, it just stores the minimun
+// and updates it when there is a new minimum 
+
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j
+      }
+    }
+    if (i !== min) {
+      [arr[min], arr[i]] = [arr[i], arr[min]]
+    }
+  }
+  return arr
+}
+
+// the time complexity of selection sort is O(n^2)
